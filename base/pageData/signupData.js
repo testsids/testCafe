1782728aae2ea88bb2d6915eb3af774d7c74b00d
testsids <qaengg9@gmail.com>
2021-01-06 21:00:46 +0530

    added signup test data

--- base/pageData/signupData.js
+++ base/pageData/signupData.js
@@ -1,10 +1,17 @@
+import { fake } from 'faker';
 import { Selector } from 'testcafe';
+
+const faker = require('faker');
 
 class signupData {
     constructor() {
-        this.retailerCompanyName = "Tesla Factory";
+        this.staticCompanyName = "Tesla Factory";
         this.password = "Testing@123";
-        
+        this.phone = faker.phone.phoneNumber();
+        this.cardNumber = "4111-1111-1111-1111";
+        this.expirydate = "12/21";
+        this.cvc = "123";
+        this.zipCode = "12345";
     }
 }
 export default new signupData()
