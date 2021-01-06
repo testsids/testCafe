5eb32f0bdfa1b21790478b72dae909c258126b83
testsids <qaengg9@gmail.com>
2021-01-06 21:01:50 +0530

    updated signup test scripts

--- testScripts/signupTest.js
+++ testScripts/signupTest.js
@@ -1,31 +1,47 @@
-import { Selector } from 'testcafe';
-import signupObject from '../Base/PageObjects/signupObjects'
-import signupData from '../Base/PageData/signupData'
-
+import signupObject from '../base/pageObjects/signupObjects'
+import signupData from '../base/pageData/signupData'
+import signupPage from '../base/pages/signupPages'
+const moment = require('moment');
+const faker = require('faker');
 
 fixture`Sign Up Flow`
     .page`https://test.brewoptix.com/session/sign-up`;
 
-var moment = require('moment');
-moment().format();
-
-var email = "QaTesting" + (Math.floor(Math.random() * 10000) + 1) + "@mailinator.com";
-var yourName = "QaTest" + moment().format("h:mm:ss");
-
 test('Retailer test scripts', async t => {
-    await t
-        .expect(signupObject.retailerCheckbox.exists).ok('', { timeout: 30000 })
-        .click(signupObject.retailerCheckbox)
-        .typeText(signupObject.companyNameInput, signupData.retailerCompanyName)
-        .pressKey('down')
-        .pressKey('enter')
-        .typeText(signupObject.nameInput, yourName)
-        .typeText(signupObject.emailAddressInput, email)
-        .typeText(signupObject.passwordInput, signupData.password)
-        .click(signupObject.signUpButton)
-        .expect(signupObject.retailerName.exists).ok('', { timeout: 30000 })
-        .expect((signupObject.retailerName.innerText)).eql(yourName)
-        .expect((signupObject.retailerEmail.innerText)).eql(email.toLocaleLowerCase());
+    const email = "QaTesting" + (Math.floor(Math.random() * 10000) + 1) + "@mailinator.com";
+    const name = "QaTest" + moment().format("h_mm_ss");
+    const companyName = faker.company.companyName();
+    await signupPage.verifyElementPresence(signupObject.retailerCheckbox);
+    await signupPage.clickOnElement(signupObject.retailerCheckbox);
+    await signupPage.signup(signupObject, signupData,name,email,companyName);
+    await signupPage.clickOnElement(signupObject.signUpButton);
+    await signupPage.verifyElementPresence(signupObject.yourName);
+    await signupPage.verifyElementText(signupObject.yourName, name);
+    await signupPage.verifyElementText(signupObject.emailAddress, email.toLocaleLowerCase());
+});
 
+test('Distributor test scripts', async t => {
+    const email = "QaTesting" + (Math.floor(Math.random() * 10000) + 1) + "@mailinator.com";
+    const name = "QaTest" + moment().format("h_mm_ss");
+    const companyName = faker.company.companyName();
+    await signupPage.verifyElementPresence(signupObject.distributorCheckbox);
+    await signupPage.clickOnElement(signupObject.distributorCheckbox);
+    await signupPage.signup(signupObject, signupData,name,email,companyName);
+    await signupPage.clickOnElement(signupObject.signUpButton);
+    await signupPage.verifyElementPresence(signupObject.yourName);
+    await signupPage.verifyElementText(signupObject.yourName, name);
+    await signupPage.verifyElementText(signupObject.emailAddress, email.toLocaleLowerCase());
+});
 
+test('Supplier test scripts', async t => {
+    const email = "QaTesting" + (Math.floor(Math.random() * 10000) + 1) + "@mailinator.com";
+    const name = "QaTest" + moment().format("h_mm_ss");
+    await signupPage.verifyElementPresence(signupObject.supplierCheckbox);
+    await signupPage.clickOnElement(signupObject.supplierCheckbox);
+    await signupPage.signup(signupObject, signupData,name,email,signupData.staticCompanyName);
+    await signupPage.paymentDetails(signupObject, signupData);
+    await signupPage.clickOnElement(signupObject.signUpButton);
+    await signupPage.verifyElementPresence(signupObject.yourName);
+    await signupPage.verifyElementText(signupObject.yourName, name);
+    await signupPage.verifyElementText(signupObject.emailAddress, email.toLocaleLowerCase());
 });
