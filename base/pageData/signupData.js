acc36c377b8fe214b4b3a9802c1c51b81b5f0ade
testsids <qaengg9@gmail.com>
2021-01-06 14:29:41 +0530

    added test data for retailer flow

---
+++ base/pageData/signupData.js
@@ -0,0 +1,10 @@
+import { Selector } from 'testcafe';
+
+class signupData {
+    constructor() {
+        this.retailerCompanyName = "Tesla Factory";
+        this.password = "Testing@123";
+        
+    }
+}
+export default new signupData()
